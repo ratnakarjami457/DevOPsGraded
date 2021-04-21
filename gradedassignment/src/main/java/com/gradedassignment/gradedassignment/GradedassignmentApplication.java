c9339a02ae611cb8f57f25b05c7fb2c792974ee7
ratnakarjami457 <82525949+ratnakarjami457@users.noreply.github.com>
2021-04-21 08:13:00 +0000

    Update GradedassignmentApplication.java

--- gradedassignment/src/main/java/com/gradedassignment/gradedassignment/GradedassignmentApplication.java
+++ gradedassignment/src/main/java/com/gradedassignment/gradedassignment/GradedassignmentApplication.java
@@ -8,6 +8,7 @@ public class GradedassignmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradedassignmentApplication.class, args);
+		System.out.println("Hello DevOps");
 	}
 
 }
